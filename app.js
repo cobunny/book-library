d6c5714438340589008a43fbcf1baca7232f5dfc
cobunny <malatanggm@gmail.com>
2016-01-14 20:24:19 -0800

    Added gulp file

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ var express = require('express');
 
 var app = express();
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
 app.get('/', function (req, res) {
-    res.send('Hello world');
+    res.send('Hello World');
 });
 
 app.get('/books', function (req, res) {
     res.send('Hello books');
 });
 
-app.listen(5000, function (err) {
-    console.log('Running server on port 5000');
+app.listen(port, function (err) {
+    console.log('Running server on port' + port);
 });
