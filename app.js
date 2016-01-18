9e88e3c72ab197f2354a9e0aee69ef87b5dc0cf2
cobunny <malatanggm@gmail.com>
2016-01-17 21:03:09 -0800

    Added sign-up form

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
+
 var app = express();
 
 var port = process.env.PORT || 5000;
@@ -12,8 +17,15 @@ var nav = [{
 
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
+var authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(session({secret: 'library'}));
+require('./src/config/passport')(app);
+
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
@@ -27,6 +39,7 @@ app.get('/', function (req, res) {
 
 app.use('/Books', bookRouter);
 app.use('/Admin', adminRouter);
+app.use('/Auth', authRouter);
 
 app.get('/books', function (req, res) {
     res.send('Hello books');
